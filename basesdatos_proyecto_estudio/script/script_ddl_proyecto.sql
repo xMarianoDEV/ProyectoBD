4aff35e02d122706e246aa6b69dec10be1721c43
facu.duete <facu.duete@gmail.com>
2024-10-02 00:10:38 -0300

    Se agregan los scripts de definicion de tablas e insercion de lote de datos.

--- basesdatos_proyecto_estudio/script/script_ddl_proyecto.sql
+++ basesdatos_proyecto_estudio/script/script_ddl_proyecto.sql
@@ -1,6 +1,216 @@
--- SCRIPT TEMA "nombre del tema"
--- DEFINNICIÓN DEL MODELO DE DATOS
+--DEFINICION DEL MODELO DE DATOS
 
-CREATE DATATABLE nombre_base;
+CREATE DATABASE ProyectoBD
 
-USE nombre_base;
+go
+
+USE ProyectoBD
+
+go
+
+-- Tabla vendedor
+CREATE TABLE vendedor
+(
+  cuit_vendedor BIGINT NOT NULL,
+  nombre_apellido VARCHAR(100) NOT NULL,
+  fecha_nacimiento DATE NOT NULL,
+  email VARCHAR(100),
+  CONSTRAINT PK_vendedor_id PRIMARY KEY (cuit_vendedor),
+  CONSTRAINT UQ_vendedor_email UNIQUE (email),
+  CONSTRAINT CK_vendedor_fecha_nacimiento CHECK ((DATEDIFF(YEAR, fecha_nacimiento, GETDATE()) >= 18) and (DATEDIFF(YEAR, fecha_nacimiento, GETDATE()) <= 100))
+);
+go
+
+-- Tabla categoria
+CREATE TABLE categoria
+(
+  id_categoria INT IDENTITY NOT NULL,
+  nombre VARCHAR(100) NOT NULL,
+  CONSTRAINT PK_categoria_id PRIMARY KEY (id_categoria),
+  CONSTRAINT UQ_categoria_nombre UNIQUE (nombre)
+);
+go
+
+-- Tabla material
+CREATE TABLE material
+(
+  id_material INT IDENTITY NOT NULL,
+  nombre VARCHAR(100) NOT NULL,
+  CONSTRAINT PK_material PRIMARY KEY (id_material),
+  CONSTRAINT UQ_material_nombre UNIQUE (nombre)
+);
+go
+
+-- Tabla metodo_pago
+CREATE TABLE metodo_pago
+(
+  id_pago INT IDENTITY NOT NULL,
+  nombre VARCHAR(100) NOT NULL,
+  CONSTRAINT PK_metodo_pago_id PRIMARY KEY (id_pago),
+  CONSTRAINT UQ_metodo_pago_nombre UNIQUE (nombre)
+);
+go
+
+-- Tabla ciudad
+CREATE TABLE ciudad
+(
+  id_ciudad INT IDENTITY NOT NULL,
+  nombre VARCHAR(200) NOT NULL,
+  CONSTRAINT PK_ciudad_id PRIMARY KEY (id_ciudad),
+  CONSTRAINT UQ_ciudad_nombre UNIQUE (nombre)
+);
+go
+
+-- Tabla producto
+CREATE TABLE producto
+(
+  id_producto INT IDENTITY NOT NULL,
+  nombre VARCHAR(100) NOT NULL,
+  descripcion VARCHAR(200),
+  precio FLOAT NOT NULL,  
+  status_publicacion VARCHAR(10) NOT NULL,  
+  fecha_publicación DATE CONSTRAINT DF_producto_fecha_publicacion DEFAULT getdate() not null, 
+  id_categoria INT NOT NULL,
+  cuit_vendedor BIGINT NOT NULL,
+  CONSTRAINT PK_producto_id PRIMARY KEY (id_producto),
+  CONSTRAINT FK_producto_vendedor FOREIGN KEY (cuit_vendedor) REFERENCES vendedor(cuit_vendedor),
+  CONSTRAINT FK_producto_categoria FOREIGN KEY (id_categoria) REFERENCES categoria(id_categoria),
+  CONSTRAINT CK_producto_status CHECK (status_publicacion IN ('activa', 'pausada')),
+  CONSTRAINT CK_producto_precio CHECK (precio > 0)
+);
+go
+
+-- Tabla cliente
+CREATE TABLE cliente
+(
+  dni INT NOT NULL,
+  nombre_apellido VARCHAR(100) NOT NULL,
+  domicilio VARCHAR(200) NOT NULL,
+  email VARCHAR(100) NOT NULL,
+  telefono BIGINT,
+  id_ciudad INT NOT NULL,
+  CONSTRAINT PK_cliente_id PRIMARY KEY (dni),
+  CONSTRAINT FK_cliente_ciudad FOREIGN KEY (id_ciudad) REFERENCES ciudad(id_ciudad),
+  CONSTRAINT UQ_cliente_email UNIQUE (email),
+  CONSTRAINT UQ_cliente_telefono UNIQUE (telefono)
+);
+go
+
+-- Tabla venta
+CREATE TABLE venta
+(
+  nro_venta INT NOT NULL,
+  nro_factura INT NOT NULL,
+  fecha_venta DATE NOT NULL,
+  dni INT NOT NULL,
+  CONSTRAINT PK_venta_id PRIMARY KEY (nro_venta),
+  CONSTRAINT UQ_venta_nro_factura UNIQUE (nro_factura),
+  CONSTRAINT FK_venta_cliente FOREIGN KEY (dni) REFERENCES cliente(dni)
+);
+go
+
+-- Tabla venta_detalle
+CREATE TABLE venta_detalle(
+  cantidad INT NOT NULL,  
+  precio_venta FLOAT NOT NULL,  
+  nro_venta INT NOT NULL,
+  id_producto INT NOT NULL,
+  CONSTRAINT PK_venta_detalle PRIMARY KEY (nro_venta, id_producto),
+  CONSTRAINT FK_venta_detalle_venta FOREIGN KEY (nro_venta) REFERENCES venta(nro_venta),
+  CONSTRAINT FK_venta_detalle_producto FOREIGN KEY (id_producto) REFERENCES producto(id_producto),
+  CONSTRAINT CK_cantidad_venta_detalle CHECK (cantidad > 0),
+  CONSTRAINT CK_precio_venta_venta_detalle CHECK (precio_venta > 0)
+);
+go
+
+-- Tabla resena
+CREATE TABLE resena
+(
+  id_resena INT IDENTITY NOT NULL,
+  calificacion INT NOT NULL,  
+  comentario VARCHAR(200) NOT NULL,
+  nro_venta INT NOT NULL,
+  id_producto INT NOT NULL,
+  CONSTRAINT PK_resena PRIMARY KEY (id_resena, nro_venta, id_producto),
+  CONSTRAINT FK_resena_venta_detalle FOREIGN KEY (nro_venta, id_producto) REFERENCES venta_detalle(nro_venta, id_producto),
+  CONSTRAINT CK_calificacion_resena CHECK (calificacion BETWEEN 1 AND 10)
+);
+go
+
+-- Tabla producto_material
+CREATE TABLE producto_material
+(
+  porcentaje INT NOT NULL,
+  id_producto INT NOT NULL,
+  id_material INT NOT NULL,
+  CONSTRAINT PK_producto_material PRIMARY KEY (id_producto, id_material),
+  CONSTRAINT FK_producto_material_producto FOREIGN KEY (id_producto) REFERENCES producto(id_producto),
+  CONSTRAINT FK_producto_material_material FOREIGN KEY (id_material) REFERENCES material(id_material),
+  CONSTRAINT CK_porcentaje_producto_material CHECK (porcentaje > 0 AND porcentaje <= 100)
+);
+go
+
+-- Tabla pago
+CREATE TABLE pago
+(
+  importe FLOAT NOT NULL,
+  nro_venta INT NOT NULL,
+  id_pago INT NOT NULL,
+  CONSTRAINT PK_pago PRIMARY KEY (nro_venta, id_pago),
+  CONSTRAINT FK_pago_venta FOREIGN KEY (nro_venta) REFERENCES venta(nro_venta),
+  CONSTRAINT FK_pago_metodo_pago FOREIGN KEY (id_pago) REFERENCES metodo_pago(id_pago),
+  CONSTRAINT CK_importe_pago CHECK (importe > 0) -- Restricción CHECK para importe positivo
+);
+go
+
+--Restricciones adicionales
+
+--Restriccion para CUIT valido de vendedores
+ALTER TABLE VENDEDOR
+ADD CONSTRAINT CK_vendedor_cuit
+CHECK (cuit_vendedor > 999999999);
+go
+
+--Restriccion para correo electronico valido de vendedores
+ALTER TABLE VENDEDOR
+ADD CONSTRAINT CK_vendedor_email
+CHECK (
+    email LIKE '%_@__%.__%' 
+    AND email NOT LIKE '%[^A-Za-z0-9@._-]%' 
+    AND CHARINDEX(' ', email) = 0
+);
+go
+
+--Restricción para nombres completos validos de vendedores
+ALTER TABLE VENDEDOR
+ADD CONSTRAINT CK_Vendedor_NombreApellido
+CHECK (nombre_apellido LIKE '%[A-Za-zÑñÁÉÍÓÚáéíóú ]%' and nombre_apellido NOT LIKE '%[^A-Za-zÑñÁÉÍÓÚáéíóú ]%')
+go
+
+--Restriccion para documentos validos de clientes
+ALTER TABLE CLIENTE
+ADD CONSTRAINT CK_cliente_dni
+CHECK (dni > 999999);
+go
+
+--Restricción para nombres completos validos de clientes
+ALTER TABLE CLIENTE
+ADD CONSTRAINT CK_Cliente_NombreApellido
+CHECK (nombre_apellido LIKE '%[A-Za-zÑñÁÉÍÓÚáéíóú ]%' and nombre_apellido NOT LIKE '%[^A-Za-zÑñÁÉÍÓÚáéíóú ]%')
+go
+
+--Restriccion para correo electronico valido de clientes
+ALTER TABLE VENDEDOR
+ADD CONSTRAINT CK_cliente_email
+CHECK (
+    email LIKE '%_@__%.__%' 
+    AND email NOT LIKE '%[^A-Za-z0-9@._-]%' 
+    AND CHARINDEX(' ', email) = 0
+);
+go
+
+--Restriccion para telefonos validos de clientes
+ALTER TABLE CLIENTE
+ADD CONSTRAINT CK_cliente_telefono
+CHECK (telefono > 9999999);
+go
--- basesdatos_proyecto_estudio/script/script_insert_proyecto.sql
+++ basesdatos_proyecto_estudio/script/script_insert_proyecto.sql
@@ -1,6 +1,87 @@
--- SCRIPT "nombre del proyecto"
 -- INSERCIÓN DEL LOTE DE DATOS
 
-INSERT INTO table_a VALUES (0,1);
-INSERT INTO table_a VALUES (1,2);
+-- Lote de datos representativo
+
+-- tabla vendedor
+INSERT INTO vendedor (cuit_vendedor, nombre_apellido, fecha_nacimiento, email) 
+VALUES 
+(30714091059, 'Juan Perez', '1980-05-20', 'juan.perez@example.com'),
+(30714064319, 'Ana Lopez', '1995-03-15', 'ana.lopez@example.com'),
+(30714052752, 'Martin Gomez', '2000-04-25', 'martin.gomez@example.com');
+
+-- tabla categoria
+INSERT INTO categoria (nombre) 
+VALUES 
+('Construcción'),
+('Decoración');
+
+
+-- tabla material
+INSERT INTO material (nombre) 
+VALUES 
+('Cemento'),
+('Piedra'),
+('Vidrio');
+
+-- tabla metodo_pago
+INSERT INTO metodo_pago (nombre) 
+VALUES 
+('Tarjeta de Crédito'),
+('Tarjeta de Débito'),
+('Efectivo');
+
+
+-- tabla ciudad
+INSERT INTO ciudad (nombre) 
+VALUES 
+('Corrientes Capital'),
+('Buenos Aires'),
+('Rosario');
+
+-- tabla producto
+INSERT INTO producto (nombre, descripcion, precio, status_publicacion, id_categoria, cuit_vendedor) 
+VALUES 
+('Ladrillo', 'Ladrillo rojo de alta calidad', 10.50, 'activa', 1, 30714091059),
+('Pintura', 'Pintura blanca para interiores', 25.00, 'pausada', 2, 30714052752),
+('Vaso', null, 35.00, 'activa', 2, 30714064319);
+
+-- tabla cliente
+INSERT INTO cliente (dni, nombre_apellido, domicilio, email, telefono, id_ciudad) 
+VALUES 
+(41583200, 'Maria Garcia', 'Calle Falsa 123', 'maria.garcia@example.com', 3794620313, 1),
+(43754259, 'Juan Lopez', 'Av. Independencia 1900', 'juanlopez@example.com', 3795624850, 1),
+(31689576, 'Carlos Gutierrez', 'Av. Siempre Viva 742', 'carlos.gutierrez@example.com', NULL, 2);
+
+-- tabla venta
+INSERT INTO venta (nro_venta, nro_factura, fecha_venta, dni) 
+VALUES 
+(1, 1001, '2024-09-10', 41583200),
+(2, 1002, '2024-09-11', 31689576);
+
+
+-- tabla venta_detalle
+INSERT INTO venta_detalle (cantidad, precio_venta, nro_venta, id_producto) 
+VALUES 
+(3, 10.50, 1, 1),
+(5, 35.00, 2, 3);
+
+
+-- tabla resena
+INSERT INTO resena (calificacion, comentario, nro_venta, id_producto) 
+VALUES 
+(8, 'Buen producto, recomendado', 1, 1),
+(7, 'Cumple su función', 2, 3);
+
+-- tabla producto_material
+INSERT INTO producto_material (porcentaje, id_producto, id_material) 
+VALUES 
+(60, 1, 1),  -- Ladrillo con Cemento
+(40, 1, 2);  -- Ladrillo con Piedra
+
+-- tabla pago
+INSERT INTO pago (importe, nro_venta, id_pago) 
+VALUES 
+(105.00, 1, 1),  -- Pago con Tarjeta de Crédito
+(125.00, 2, 3);  -- Pago en Efectivo
+
 
